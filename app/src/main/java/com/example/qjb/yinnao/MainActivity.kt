d2b6886e9435b03c95eaa74536931e6d3cce1ff3
airqj <gugeqinjianbo@gmail.com>
2017-10-31 18:28:36 +0800

    auto scroll by timer

--- app/src/main/java/com/example/qjb/yinnao/MainActivity.kt
+++ app/src/main/java/com/example/qjb/yinnao/MainActivity.kt
@@ -1,21 +1,40 @@
 package com.example.qjb.yinnao
 
+import android.content.res.Configuration
+import android.graphics.Color.*
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.ScrollView
 import android.view.View.OnClickListener
 import android.widget.Button
+import android.widget.ImageButton
+import bolts.Task
 import com.github.piasy.biv.BigImageViewer
 import com.github.piasy.biv.view.BigImageView
 import com.github.piasy.biv.loader.fresco.FrescoImageLoader
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity(),OnClickListener {
 
     private var mBigImageView:BigImageView? = null
     private var mScrollView:ScrollView? = null
-    private var mBtnTest:Button? = null
+    private var mBtnTest:ImageButton? = null
+    private var timer = Timer()
+    private var mBtnTestStatus:Boolean = false
+
+    fun doAddY() {
+        if(mScrollView?.bottom!! >= mBigImageView?.bottom!!) {
+            mScrollView?.scrollTo(0, mScrollView?.scrollY!!.plus(1))
+            System.out.println("Time Out")
+        }
+        else {
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +50,18 @@ class MainActivity : AppCompatActivity(),OnClickListener {
         mBigImageView?.showImage(Uri.parse("http://ww1.sinaimg.cn/mw690/005Fj2RDgw1f9mvl4pivvj30c82ougw3.jpg"))
     }
 
-    override fun onClick(p0: View?) {
-        if(p0?.id == mBtnTest?.id) {
-            val yCurrent = mScrollView?.pivotY?.toInt()
-            mScrollView?.scrollTo(0,mScrollView?.scrollY!!.plus(1))
+    override fun onClick(v: View?) {
+        if(v?.id == mBtnTest?.id) {
+            if(!mBtnTestStatus) {
+                System.out.println("Btn Click")
+                timer.schedule(timerTask { doAddY() }, 1000, 100)
+                mBtnTestStatus = true
+            }
+            else {
+                timer.cancel()
+                mBtnTestStatus = false
+                timer = Timer()
+            }
         }
     }
 }
